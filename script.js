1eeb09cda8cf513de055ded71d08a53a37cb8a99
NightCoder <63603308+superebot10@users.noreply.github.com>
2021-08-06 11:40:39 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function changeClass(){
+    document.getElementById("changeClass").className = "aqua";
+}
+function changeText(){
+    document.getElementById("changeText").innerHTML = "You changed this text!"
+}
